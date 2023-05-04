63fa55b9cd810351108472f2c39159040e496c32
maulikdomdiya <132455402+maulikdomdiya@users.noreply.github.com>
2023-05-04 15:21:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,23 @@ const app = express();
 
 const path = require('path');
 
+const mongoose = require('mongoose');
+
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views'));
+
+mongoose.connect('mongodb+srv://maulikpatel4723:mdg.1234@cluster0.8z1c8ed.mongodb.net/test',{
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+}).then(()=>{
+    console.log('DB Connect');
+}).catch(err =>{
+    console.log('DB Not Connected !!',err);
+})
 
 app.use(express.urlencoded());
 app.use('/upload',express.static(path.join(__dirname+'/upload')));
 
-const db = require('./confing/mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportlocal = require('./confing/passport-local-strategy');
